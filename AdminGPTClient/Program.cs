278ead60006a15234b566797f039acef9ab6ba77
Jens Caasen <jenscaasen@gmail.com>
2023-04-21 18:17:13 +0200

    Updated Sample Client to be general purpose and added powershell support

--- AdminGPTClient/Program.cs
+++ AdminGPTClient/Program.cs
@@ -1,8 +1,13 @@
 ﻿using AdminGPT;
+using Azure;
 using Azure.Identity;
 using Newtonsoft.Json;
 using Renci.SshNet;
+using Renci.SshNet.Security;
+using System.Collections.ObjectModel;
 using System.Dynamic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Net.Http;
 using System.Text;
 
@@ -11,6 +16,7 @@ namespace AdminGPTClient
     internal class Program
     {
         private static SshClient sshClient;
+        private static Runspace powershell;
         static string apiKey = string.Empty;
 
 
@@ -20,8 +26,29 @@ namespace AdminGPTClient
             apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
             try
             {
-               // GraphAdminExample();
-                 LinuxAdminExample();
+                Console.WriteLine("Welcome to AdminGPT, please select a technology to start with:");
+                Console.WriteLine("1. Microsoft Graph");
+                Console.WriteLine("2. Linux SSH");
+                Console.WriteLine("3. Local PowerShell");
+                Console.Write("Your choice: ");
+                var choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    GraphAdminExample();
+                }
+                else if (choice == "2")
+                {
+                    LinuxAdminExample();
+                }
+                else if (choice == "3")
+                {
+                    LocalPowerShellExample();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, exiting ...");
+                }
+              
             }
             catch (Exception ex)
             {
@@ -30,8 +57,75 @@ namespace AdminGPTClient
           
         }
 
+        private static void LocalPowerShellExample()
+        {
+            powershell = RunspaceFactory.CreateRunspace();
+            powershell.Open();
+
+            Console.WriteLine("Please note that only non-interactive PowerShell sessions are supported");
+            Console.WriteLine("-----");
+            Console.WriteLine("AdminGPT: Hello, how can i help you with PowerShell?");
+            Console.Write("You: ");
+            string userCommand = Console.ReadLine();
+
+            AdminGPTBot adminBot = new AdminGPTBot(apiKey);
+            var powerShellSuggestion = (PowerShellWhatToRunSuggestion)adminBot.Start(userCommand, AdminGptTechnology.POWERSHELL);
+
+           while(true)
+            {
+                UserResult result = new UserResult();
+                Console.WriteLine("AdminGPT: " + powerShellSuggestion.Explanation);
+                if (powerShellSuggestion.SuggestionType == SuggestionType.QueryUser)
+                {
+                    Console.WriteLine("[Question] AdminGPT: " + powerShellSuggestion.UserPrompt);
+                    Console.Write("You: ");
+                    string userPromt = Console.ReadLine();
+                    result.ActualPrompt = powerShellSuggestion.UserPrompt;
+                    result.ResultText = userPromt;
+                }
+                else
+                {
+                    Console.WriteLine($"[Execution] AdminGPT: {powerShellSuggestion.SuggestedPowerShellPrompt}");
+                    Console.WriteLine("[Press any key to run the above suggestion]");
+                    Console.ReadKey();
+                    KillLastLine();
+                    string powerShellRunResult = RunPowerShellAndGetResult(powerShellSuggestion);
+                    result.ActualPrompt = powerShellSuggestion.SuggestedPowerShellPrompt;
+                    result.ResultText = powerShellRunResult;
+                    Console.WriteLine("PowerShell: " + powerShellRunResult);
+                }
+                Console.WriteLine("[Asking AdminGPT for the next step, please wait ...]");
+                powerShellSuggestion = (PowerShellWhatToRunSuggestion)adminBot.Next(result);
+                KillLastLine();
+            }
+        }
+
+        private static string RunPowerShellAndGetResult(PowerShellWhatToRunSuggestion powerShellSuggestion)
+        {
+            string result = string.Empty;
+            using (Pipeline pipeline = powershell.CreatePipeline())
+            {
+                pipeline.Commands.AddScript(powerShellSuggestion.SuggestedPowerShellPrompt);
+                // Execute the command and return results 
+               
+                try
+                {
+                    Collection<PSObject> results = pipeline.Invoke();
+
+                    result = string.Join(Environment.NewLine, results.Select(r => r.ToString()));
+                }
+                catch (Exception e)
+                {
+                    result = e.Message;
+                }
+            }
+            if(result == string.Empty ) { result = "(Empty Result)"; }
+            return result;
+        }
+
         private static void GraphAdminExample()
         {
+            Console.WriteLine("Please make sure you have an Azure Credentials set up, see https://docs.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential?view=azure-dotnet for more details.");
             Console.WriteLine("-----");
             Console.WriteLine("AdminGPT: Hello, how can i help you with MS Graph?");
             Console.Write("You: ");
@@ -111,13 +205,26 @@ namespace AdminGPTClient
 
         private static void LinuxAdminExample()
         {
-            string host = "192.168.178.108";
-            string username = "pi";
-            string password = Environment.GetEnvironmentVariable("PI_SSH_PASSWORD");
-
-            Console.WriteLine("Using predefined ssh host: '" + host + "' ...");
+            Console.WriteLine("Please enter your ssh host (default is 192.168.178.108):");
+            string host = Console.ReadLine();
+            if(string.IsNullOrEmpty(host))
+            {
+                host = "192.168.178.108";
+            }
+            Console.WriteLine("Please enter your ssh username (e.g. pi):");
+            string username = Console.ReadLine();
+            if(string.IsNullOrEmpty(username))
+            {
+                username = "pi";
+            }
+            Console.WriteLine("Please enter your ssh password (default uses 'PI_SSH_PASSWORD' environmment variable):");
+            string password = Console.ReadLine();
+           if(string.IsNullOrEmpty(password))
+            {
+                password = Environment.GetEnvironmentVariable("PI_SSH_PASSWORD");
+            }
+            
             Console.WriteLine("Using predefined apiKey: '" + apiKey.Substring(0, 8) + "' ...");
-            Console.WriteLine("Starting ssh client with predefined user '" + username + "' ...");
             sshClient = new SshClient(host, username, password);
             sshClient.Connect();
             Console.WriteLine("Connected to ssh host: '" + host + "' ...");
